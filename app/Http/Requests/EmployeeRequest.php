afd30dda59bd4854666f9a85c33ee6a506148a4a
Ghazamalghifari <ghozam.alghifari96@gmail.com>
2022-05-19 00:30:36 +0700

    create employee (update)

--- app/Http/Requests/EmployeeRequest.php
+++ app/Http/Requests/EmployeeRequest.php
@@ -27,10 +27,10 @@ class EmployeeRequest extends FormRequest
         return [
           'name.string' => 'Name harus berupa string..',
           'name.required' => 'Name wajib diisi..',
-          'name.min:2' => 'Name minimal 2 karakter..',
+          'name.min' => 'Name minimal 2 karakter..',
           'name.unique:employees' => 'Name sudah ada sebelumnya..',
           'salary.integer' => 'Salary harus berupa bilangan bulat..',
-          'salary.between:2000000,10000000' => 'Salary Minimal 2.000.000 Maximal 10.000.000',
+          'salary.between' => 'Salary Minimal 2.000.000 Maximal 10.000.000',
         ];
     }
 }
